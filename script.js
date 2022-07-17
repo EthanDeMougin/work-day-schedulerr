75e07382fa8b4b28e687fff7153ea9780591f30a
Ethan DeMougin <ethandemougin@gmail.com>
2022-07-17 11:11:32 -0400

    timeslot colors

---
+++ script.js
@@ -0,0 +1,20 @@
+var current_task_color = 'rgb(255,185,97,0.8)';
+var future_task_color = 'rgb(119,221,119,0.8)';
+function setTimeSlotColors() {
+    var currentTime = moment().format('kk');
+    for (var i = 0; i < timeSlots.length; i++) {
+        if (parseInt(timeSlots[i].id) == currentTime) {
+            timeSlots[i].style.backgroundColor = current_task_color;
+        } else if (parseInt(timeSlots[i].id) > currentTime) {
+            timeSlots[i].style.backgroundColor = future_task_color;
+        }
+    }
+}
+
+
+var currentDate = moment().format('dddd, MMM Do');
+$("#currentDay").text(currentDate);
+
+var timeSlots = $(".to-do").toArray();
+
+setTimeSlotColors(); 
