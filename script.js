2640b3bf4e8eb1968e6734f1a34d72cd9a020be4
Ethan DeMougin <ethandemougin@gmail.com>
2022-07-24 15:50:14 -0400

    added mass delete button and checks

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var future_task_color = 'rgb(119,221,119,0.8)';
 function setTimeSlotColors() {
     var currentTime = moment().format('kk');
     for (var i = 0; i < timeSlots.length; i++) {
-        if (parseInt(timeSlots[i].id) == currentTime) {
+        if (parseInt(timeSlots[i].id) == (currentTime - 4)) {
             timeSlots[i].style.backgroundColor = current_task_color;
         } else if (parseInt(timeSlots[i].id) > currentTime) {
             timeSlots[i].style.backgroundColor = future_task_color;
@@ -23,9 +23,24 @@ function getStoredTasks() {
         tasks = JSON.parse(localStorage.getItem("tasks"));
         console.log(tasks);
     }
+    $(".display-text").each(function (index, element) {
+        for (var i = 0; i < tasks.length; i++) {
+            if (tasks[i].pID == element.id) {
+                element.textContent = tasks[i].task;
+            }
+        }
+    })
 }
 
+
+
+
+
 $(".to-do").on("click", function () {
+    $(".save-button").each(function (index, element) {
+        element.style.opacity = 0.2;
+    })
+    $(this).parent().children(".save-button")[0].style.opacity = 1;
     $(".form-control").each(function (index, element) {
         element.style.visibility = "hidden";
     })
@@ -34,6 +49,9 @@ $(".to-do").on("click", function () {
 });
 
 $(".save-button").on("click", function () {
+    $(".save-button").each(function (index, element) {
+        element.style.opacity = (0.7);
+    })
     var currentTaskForm = $(this).parent().children(".to-do").children(".form-control")[0];
     var taskDescription = currentTaskForm.value;
     var currentP = $(this).parent().children(".to-do").children(".display-text")[0];
@@ -48,6 +66,11 @@ $(".save-button").on("click", function () {
     tasks.push(currentTask);
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
+});
+
+$("#remove-tasks").on("click", function () {
+    localStorage.clear();
+    location.reload();
 });
 
 getStoredTasks();
